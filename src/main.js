9ad9e064999f276f4c6178fd0650c8952744a044
tangxun <tangxun@dataexa.com>
2020-04-24 19:58:12 +0800

    新建项目及加载进度条

--- src/main.js
+++ src/main.js
@@ -3,8 +3,18 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import nprogress from "nprogress"; // 导入网页加载进度条
+import 'nprogress/nprogress.css';　// 导入网页加载进度条
 
 Vue.config.productionTip = false
+
+router.beforeEach((to, from, next) => {
+  nprogress.start();
+  next();  
+})
+router.afterEach( route => {
+  nprogress.done();  
+})
 
 /* eslint-disable no-new */
 new Vue({
